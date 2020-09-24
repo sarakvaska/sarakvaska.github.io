683c50d33a6a70cf155ba0e0ee8d93c09d3806b7
Sara Kvaska <sarakvaska@gmail.com>
2020-09-23 21:26:23 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sarakvaska.github.io/precache-manifest.d5533f8a6d8cdb9a370fe3761a29c663.js"
+  "/sarakvaska.github.io/precache-manifest.fea092593ef61544401870014e54fa04.js"
 );
 
 self.addEventListener('message', (event) => {
